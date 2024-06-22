2aab9e75234485cc8c8dc1b9ecf74dcd16f1bcdf
sS_Tr33s <24_de_7@protonmail.com>
2024-06-22 12:49:56 -0500

    configuración base
    settings dividico en base local y prod

---
+++ gestionUsarios/settings/local.py
@@ -0,0 +1,21 @@
+from .base import *
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5431',
+    }
+}
+
+STATIC_URL = '/static/'
